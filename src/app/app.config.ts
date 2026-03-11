695a584b55d2542f6b466b3b630afc79d4be09ca
hbtlli <heloisabertolli@gmail.com>
2026-03-11 15:01:34 -0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
+
