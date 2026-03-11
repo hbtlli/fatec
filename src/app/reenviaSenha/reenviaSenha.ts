327a3b6b39422f801d0d5ff07dcd1ff6c20ea939
hbtlli <heloisabertolli@gmail.com>
2026-03-11 15:00:34 -0300

    Update reenviaSenha.ts

--- src/app/reenviaSenha/reenviaSenha.ts
+++ src/app/reenviaSenha/reenviaSenha.ts
@@ -1,1 +1,34 @@
+import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
 
+@Component({
+  selector: 'app-reenviaSenha',
+  templateUrl: './reenviaSenha.html'
+})
+export class ReenviaSenha {
+
+  email: string = "";
+
+  reenviar(){
+
+    let dados = localStorage.getItem("cliente");
+
+    if(dados){
+
+      let cliente: Cliente = JSON.parse(dados);
+
+      if(cliente.email == this.email){
+
+        alert("Sua senha é: " + cliente.senha);
+
+      } else {
+
+        alert("Email não encontrado");
+
+      }
+
+    }
+
+  }
+
+}
