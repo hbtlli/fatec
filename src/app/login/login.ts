2f54ce44d474cc5541f7e18b74cd3006c57b716b
hbtlli <heloisabertolli@gmail.com>
2026-03-11 14:59:58 -0300

    Update login.ts

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,1 +1,35 @@
+import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
 
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html'
+})
+export class Login {
+
+  email: string = "";
+  senha: string = "";
+
+  login(){
+
+    let dados = localStorage.getItem("cliente");
+
+    if(dados){
+
+      let cliente: Cliente = JSON.parse(dados);
+
+      if(cliente.email == this.email && cliente.senha == this.senha){
+
+        alert("Login realizado com sucesso");
+
+      } else {
+
+        alert("Email ou senha incorretos");
+
+      }
+
+    }
+
+  }
+
+}
