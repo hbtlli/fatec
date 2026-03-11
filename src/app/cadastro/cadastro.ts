934549d942168d649acac78a3966f9d69128a9e8
hbtlli <heloisabertolli@gmail.com>
2026-03-11 14:59:29 -0300

    Update cadastro.ts

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,1 +1,22 @@
+import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
 
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.html'
+})
+export class Cadastro {
+
+  cliente: Cliente = new Cliente();
+
+  salvar(){
+
+    localStorage.setItem(
+      "cliente",
+      JSON.stringify(this.cliente)
+    );
+
+    alert("Cliente cadastrado com sucesso!");
+  }
+
+}
